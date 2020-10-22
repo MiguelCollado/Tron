085269087858fad92729b9e818b25624f99ae64e
Miguel <ironmike@jaenjam.com>
2020-10-22 20:43:57 +0200

    Logging

--- Tron/src/Tron.h
+++ Tron/src/Tron.h
@@ -3,6 +3,7 @@
 // Tron Header -> Uso exclusivo para aplicaciones Tron
 
 #include "Tron/Application.h"
+#include "Tron/Log.h"
 
 // ------ Entry Point ------
 #include "Tron/EntryPoint.h"
---
+++ Tron/src/Tron/Log.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Tron {
+
+	class TRON_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros
+#define TN_CORE_TRACE(...)  ::Tron::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define TN_CORE_INFO(...)   ::Tron::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define TN_CORE_WARN(...)   ::Tron::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define TN_CORE_ERROR(...)  ::Tron::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define TN_CORE_FATAL(...)  ::Tron::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define TN_TRACE(...)  ::Tron::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define TN_INFO(...)   ::Tron::Log::GetClientLogger()->info(__VA_ARGS__)
+#define TN_WARN(...)   ::Tron::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define TN_ERROR(...)  ::Tron::Log::GetClientLogger()->error(__VA_ARGS__)
+#define TN_FATAL(...)  ::Tron::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
+
+
+
